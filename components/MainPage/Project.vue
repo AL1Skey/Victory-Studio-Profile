03c8faba870de0fed177550100cd61f4203e4b17
ZAINU FIRDAUS IMANSYAH <zazain031@gmail.com>
2024-11-26 20:41:58 +0700

    fix:motion

--- components/MainPage/Project.vue
+++ components/MainPage/Project.vue
@@ -7,13 +7,19 @@
             <div class="row align-items-center">
                 <div class="col-xl-4">
                     <div class="project-content">
-                        <div class="section-title white-title mb-30" data-aos="fade-right">
+                        <div
+                            class="section-title white-title mb-30"
+                            data-aos="fade-right"
+                        >
                             <span class="sub-title">Our Complete Craft</span>
                             <h2 class="title">{{ preproject[0].title }}</h2>
                         </div>
-                        <p data-aos="fade-right">{{ preproject[0].description }}</p>
+                        <p data-aos="fade-right ">
+                            {{ preproject[0].description }}
+                        </p>
                         <div class="content-bottom">
-                            <a data-aos="fade-right"
+                            <a
+                                data-aos="fade-right"
                                 href="https://api.whatsapp.com/send?phone=6281930456886&text=Halo%20saya%20tertarik%20dengan%20jasa%20pembuatan%20website."
                                 class="btn"
                                 >Contact Us <span></span
@@ -27,7 +33,10 @@
                 </div>
                 <div class="col-xl-8">
                     <div class="project-item-wrap">
-                        <div class="swiper-container project-active tw-h-400" data-aos="fade-left">
+                        <div
+                            class="swiper-container project-active tw-h-400"
+                            data-aos="fade-left"
+                        >
                             <div class="swiper-wrapper">
                                 <div class="swiper-slide" v-for="i in data">
                                     <div class="project-item">
@@ -61,9 +70,8 @@
 </template>
 
 <script lang="ts">
-
 import AOS from "aos";
-import "aos/dist/aos.css"
+import "aos/dist/aos.css";
 import { register } from "swiper/element/bundle";
 import Swiper from "swiper";
 import { onMounted } from "vue";
@@ -73,38 +81,36 @@ import project from "@/assets/json/project.json";
 register();
 
 export default {
-  setup() {
-    const data = project;
-    const preproject = projecttext;
-
-
+    setup() {
+        const data = project;
+        const preproject = projecttext;
 
         onMounted(() => {
             AOS.init({
-        duration: 600, // Animation duration in milliseconds
-        once: false, // Allow animations to repeat on scroll
-        easing: "ease-in-out", // Smooth animation easing
-        anchorPlacement: "top-bottom", // Specifies animation start point
-    });
-      const swiper = new Swiper('.project-active', {
-        loop: true, // Enables looping
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-        slidesPerView: 4,
-        spaceBetween: 230,
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
-        }
-      });
-    });
+                duration: 600, // Animation duration in milliseconds
+                once: false, // Allow animations to repeat on scroll
+                easing: "ease-in-out", // Smooth animation easing
+                anchorPlacement: "top-bottom", // Specifies animation start point
+            });
+            const swiper = new Swiper(".project-active", {
+                loop: true, // Enables looping
+                navigation: {
+                    nextEl: ".swiper-button-next",
+                    prevEl: ".swiper-button-prev",
+                },
+                slidesPerView: 4,
+                spaceBetween: 230,
+                autoplay: {
+                    delay: 2500,
+                    disableOnInteraction: false,
+                },
+            });
+        });
 
-    return {
-      data,
-      preproject,
-    };
-  },
+        return {
+            data,
+            preproject,
+        };
+    },
 };
 </script>
